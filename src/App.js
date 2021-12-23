64874400a223f8d4f8f3147bb63b6bbb0cc5d3bf
Adarsh <adarshgupta2468@gmail.com>
2021-12-24 00:23:10 +0530

    scrollbar

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import {
   
 } from "react-router-dom";
 import Blogs from "./components/blogs";
+import "./App.css"
 
 function App() {
   return (
@@ -30,7 +31,7 @@ function App() {
           </Route>
           <Route path="/">
          
-    <div style={{ overflowX: "hidden" }}>
+    <div className="aapp_scro" style={{ overflowX: "hidden" }}>
 
 
 
